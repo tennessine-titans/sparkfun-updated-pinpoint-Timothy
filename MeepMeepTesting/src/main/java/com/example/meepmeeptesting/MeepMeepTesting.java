4203428253ccde3ff8ba8cf0097042e86cb5817e
tennessine-titans <tennessinetitans@gmail.com>
2025-06-01 17:07:34 -0700

    Fixed MeepMeep
    Now works with RR1.0

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,37 +1,45 @@
 package com.example.meepmeeptesting;
-import androidx.annotation.NonNull;
+//import androidx.annotation.NonNull;
+import com.acmerobotics.roadrunner.Pose2d;
+//import com.acmerobotics.roadrunner.geometry.Vector2d;
+//import com.acmerobotics.roadrunner.path.PathBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
 
-
-
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-
-import com.acmerobotics.roadrunner.geometry.Vector2d;
-import com.acmerobotics.roadrunner.path.PathBuilder;
-import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
-import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
     public static void main(String[] args) {
-        MeepMeep meepMeep = new MeepMeep(800);
+        MeepMeep meepMeep = new MeepMeep(640);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                        .
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
-                        .forward(30)
-                        .turn(Math.toRadians(90))
+                .setConstraints(45, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                        .build();
+                myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-9, 64, 3*Math.PI/2))
+                        .strafeToLinearHeading(new Vector2d(-7,30),3*Math.PI/2)
+                        .setTangent(3*Math.PI/4)
+                        .splineToConstantHeading(new Vector2d(-30, 40),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-38, 14),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-45, 58),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-48, 14),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-55, 58),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-58, 14),Math.PI)
+                        .splineToConstantHeading(new Vector2d(-60, 16),Math.PI/2)
+                        .splineToConstantHeading(new Vector2d(-55, 58),0)
                         .build());
 
+        Image img = null;
+        try { img = ImageIO.read(new File("C:\\Users\\FTC21457\\Pictures\\field-2024-juice-dark.png")); }
+        catch(IOException e) {}
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+        meepMeep.setBackground(img)
+        //meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(myBot)
